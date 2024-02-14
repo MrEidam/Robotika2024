d02ff2c89b0596b2a384cf62d97871e5425ef2f0
MrEidam <adammbrich@gmail.com>
2024-02-14 16:14:53 +0100

    Added version 1.2 of le David

--- David.c
+++ David.c
@@ -5,33 +5,61 @@
 #include "JoystickDriver.c"
 
 //	F5 -> Start -> Robot -> Lego Brick -> Joystick control BASIC
+int limitSpeed = 50;
+// int maxRotate = 90;
+// int pawStatus = 0;
+// int pstop = 0;
 
-int limitSpeed = 20;
-int maxRotate = 90;
-int paws = false; // true = open | false = close
+void encspeedR(int rot, int tolerance){
+    int encoder = getMotorEncoder(motorPR);
+    int diff = rot-encoder;
+    int nspeed = diff/2;
+    
+    if(abs(diff) > tolerance){
+        setMotorSpeed(motorPR, nspeed);
+    }else{
+        setMotorSpeed(motorPR, 0);
+    }
+	displayTextLine(8, "diff: %i, sped: %i", diff, nspeed);
+}
+
+void encspeedL(int rot, int tolerance){
+    int encoder = getMotorEncoder(motorPL);
+    int diff = rot-encoder;
+    int nspeed = diff/2;
+    
+    if(abs(diff) > tolerance){
+        setMotorSpeed(motorPL, nspeed);
+    }else{
+        setMotorSpeed(motorPL, 0);
+    }
+}
 
 void popen(){
-	paws = true;
-	moveMotorTarget(motorPR, maxRotate, 100);
-	delay(250);
-	moveMotorTarget(motorPL, maxRotate, 100);
+	encspeedR(90,10);
 	delay(100);
+	encspeedL(90,10);
+	delay(50);
+}
+
+void p45(){
+	encspeedR(55,10);
+	encspeedL(35,10);
 }
 
 void pclose(){
-	paws = false;
-	moveMotorTarget(motorPL, -maxRotate, -100);
-	delay(250);
-	moveMotorTarget(motorPR, -maxRotate, -100);
+	encspeedL(0,10);
 	delay(100);
+	encspeedR(0,10);
+	delay(50);
 }
 
 task main(){
+	resetMotorEncoder(motorPR);
+	resetMotorEncoder(motorPL);
 	while(true){
 		getJoystickSettings(joystick);
 
-		resetMotorEncoder(motorPR);
-		resetMotorEncoder(motorPL);
 
 		if(joystick.joy1_y1>limitSpeed || joystick.joy1_y1<-limitSpeed || joystick.joy1_x2>limitSpeed || joystick.joy1_x2<-limitSpeed){
 			setMotorSpeed(motorR, -(joystick.joy1_y1/2)+(joystick.joy1_x2/2));
@@ -50,17 +78,16 @@ task main(){
 		displayTextLine       (7, "Battery: %i", nAvgBatteryLevel);
 		displayBigTextLine			(12, " And I'm David");
 
-		if(joy1Btn(5)||joy1Btn(6)||joy1Btn(7)||joy1Btn(8)){
-			if((joy1Btn(5)||joy1Btn(7)) && (paws == false))popen();
-			if((joy1Btn(6)||joy1Btn(8)) && (paws == true))pclose();
+		if(joy1Btn(4)||joy1Btn(5)||joy1Btn(6)||joy1Btn(7)||joy1Btn(8)){
+			if(joy1Btn(5)||joy1Btn(7))popen();
+			if(joy1Btn(4))p45();
+			if(joy1Btn(6)||joy1Btn(8))pclose();
 		}else{
 			setMotorSpeed(motorPR, 0);
 			setMotorSpeed(motorPL, 0);
 		}
 
-
-
-
+		
 		delay(100);
 
 //	Honk
